6dc1ca4ee579e0b367f923f6b8c72b0cd86c660b
pafcioc22 <pafcioc22@o2.pl>
2019-04-02 20:19:46 +0200

    statusCOnverter

--- App2/App2/Model/StatusConverter.cs
+++ App2/App2/Model/StatusConverter.cs
@@ -11,7 +11,7 @@ namespace App2.Model
          
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? "Sended.png" : "NotSended.png";
+            return (bool)value ? "Sended.png" : "NotSended.png" ;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
