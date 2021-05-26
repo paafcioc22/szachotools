bffaf34a0d48f6041da1f4da6c2ff5a94d7b78a0
pafcioc22 <pafcioc22@o2.pl>
2021-05-26 09:52:53 +0200

    test

--- App2/App2.Android/obj/Release/90/android/environment.arm64-v8a.s
+++ App2/App2.Android/obj/Release/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"fd48cb6e-1a6e-41f3-91da-37e54b0113e0"
+	.asciz	"04779a09-e448-4653-9fef-c20d3ae71057"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
