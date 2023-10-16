25eb02e909406afa7439210e28442932830dc39b
pafcioc22 <pafcioc22@o2.pl>
2023-10-16 16:09:13 +0200

    v51 uprawnienia bt  camera poprawa

---
+++ App2/App2/Model/IBluetoothPermissionService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App2.Model
+{
+    /// <summary>
+    /// We need to create an interface for DependencyService (Android-iOS-UWP)
+    /// </summary>
+    public interface IBluetoothPermissionService
+    {
+        Task<bool> CheckAndRequestBluetoothPermissionAsync();
+    }
+}
